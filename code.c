09746738bfefa8c51c82b9034a5bf9c54fcc8565
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-22 23:41:12 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+long decimalToBinary(int decimalnum)
+{
+    long binarynum = 0;
+    int rem, temp = 1;
+  
+    return binarynum;
+}
+
+int main()
+{
+    int decimalnum;
+    printf("Enter a Decimal Number: ");
+    scanf("%d", &decimalnum);
+    printf("Equivalent Binary Number is: %ld", decimalToBinary(decimalnum));
+    return 0;
+}
